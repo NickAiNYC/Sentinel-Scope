c0dd7de2ee78c5f99ffb41873aa0a0f1a0caa505
Nickaltstein@gmail.com <nickaltstein@gmail.com>
2025-12-27 22:21:33 -0500

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,10 @@
-from .dob_engine import DOBEngine
+from .dob_engine import DOBEngine, fetch_live_dob_alerts
 from .gap_detector import ComplianceGapEngine
 from .processor import SentinelBatchProcessor
 
 __all__ = [
     'DOBEngine', 
+    'fetch_live_dob_alerts',
     'ComplianceGapEngine', 
     'SentinelBatchProcessor'
 ]
